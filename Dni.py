81b9caa9efb7a090cd946aca3e4d1102dfc36b23
Roquedearcos <92156380+Roquedearcos@users.noreply.github.com>
2021-10-08 13:07:52 +0200

    Update Dni.py

--- Dni.py
+++ Dni.py
@@ -1,6 +1,3 @@
 def dni(numero):
   resultado=["T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"]
   return (resultado[numero%23])
-print("Este programa calcula la letra del DNI.")
-numero=int(input("Introduce tu número de DNI: "))
-print("Tu letra de DNI es la {}.".format(dni(numero)))
